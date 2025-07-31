50a2814ded48c7581fcb0a9a6e71917d574f437a
OlehSelin99 <givemetheweekend@gmail.com>
2025-07-31 14:35:13 +0300

    create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-// export const App = () => <h1>Hi</h1>
+import React from "react"
+import { Flex, Layout } from "antd"
 
-// import React from 'react';
-import { DatePicker } from 'antd';
+const { Header, Footer, Content } = Layout
 
-const App = () => {
-  return <DatePicker />;
-};
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+}
 
-export default App;
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#0958d9",
+}
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#4096ff",
+}
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: "hidden",
+  width: "100%",
+  height: "100vh",
+}
+
+const App: React.FC = () => (
+  <Flex gap="middle" wrap>
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Product List Web App</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>by @oleh__selin</Footer>
+    </Layout>
+  </Flex>
+)
+
+export default App
